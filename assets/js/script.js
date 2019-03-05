a3764577af9fa300352bb8586a1d8273b4307569
Depoule <jpoulet02100@gmail.com>
2019-03-05 11:46:36 +0100

    projet angular

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Démarrage du module angularJS
-var retroPlay = angular.module('appEcommerce', ['ngRoute']);
+var retroPlay = angular.module('retroPlay', ['ngRoute']);
 
 retroPlay.run(['$rootScope', function($rootScope){
   // Ici tableau vide
-  $rootScope.array = [];
+  $rootScope.basket = [];
 
 }]);
 // controller de ma page d'accueil avec juste ma navbar et ma fenêtre modal (j'appelle mon json)
@@ -13,26 +13,26 @@ retroPlay.controller('homeCtrl', function($scope , $http, $rootScope){
   });
 
   // au click "acheter" je récupère les info pour mettre dans mon panier
-  $rootScope.buy = function(id){
+  $scope.buy = function(id){
     // je récupère du json les données qui m'interessent
     $scope.name = $scope.products[id].name;
     $scope.support = $scope.products[id].support;
     $scope.price = $scope.products[id].price;
     $scope.quantity = $scope.products[id].quantity;
     // je push dans mon tableau les info pour mon panier
-    $rootScope.array.push({name : $scope.name, support : $scope.support, price : $scope.price, quantity : $scope.quantity});
+    $rootScope.basket.push({name : $scope.name, support : $scope.support, price : $scope.price, quantity : $scope.quantity});
   };
 
   // fonction pour add ou soustraire la quantité
   $scope.addQuantity = function(index){
-    $rootScope.array[index].quantity++;
+    $rootScope.basket[index].quantity++;
   };
   $scope.decreaseQuantity = function(index){
-    // on récup la quantité de notre tableau avec $rootScope.array[index].quantity!!!!
-    if ($rootScope.array[index].quantity > 1){
-      $rootScope.array[index].quantity--;
+    // on récup la quantité de notre tableau avec $rootScope.basket[index].quantity!!!!
+    if ($rootScope.basket[index].quantity > 1){
+      $rootScope.basket[index].quantity--;
     } else {
-      $scope.remove($rootScope.array[index]);
+      $scope.remove($rootScope.basket[index]);
     }
   };
 
@@ -40,7 +40,7 @@ retroPlay.controller('homeCtrl', function($scope , $http, $rootScope){
   // fonction pour enlever des produits de mon panier
   $scope.remove = function(index) {
     // le chiffre un me permet d'enlever un produit à la fois
-    $rootScope.array.splice(index, 1);
+    $rootScope.basket.splice(index, 1);
   };
 
 
@@ -57,25 +57,25 @@ retroPlay.config(function($routeProvider) {
     templateUrl: 'view/products.html',
     controller: 'homeCtrl'
   })
-  .when('/manette', {
-    templateUrl: 'view/manette.html',
-    controller: 'manetteCtrl'
+  .when('/pad', {
+    templateUrl: 'view/pad.html',
+    controller: 'padCtrl'
   })
-  .when('/console', {
-    templateUrl: 'view/console.html',
+  .when('/consoles', {
+    templateUrl: 'view/consoles.html',
     controller: 'consolesCtrl'
   });
 });
 
-// Mon controlleur pour la catégorie manette , je rappelle mon json
-retroPlay.controller('manetteCtrl', function($scope , $http, $rootScope){
+// Mon controlleur pour la catégorie pad , je rappelle mon json
+retroPlay.controller('padCtrl', function($scope , $http, $rootScope){
 
   $http.get('products.json').then(function(response){
     $scope.products = response.data;
   });
 });
 
-// Mon controlleur pour la catégorie console, je rappelle mon json
+// Mon controlleur pour la catégorie consoles, je rappelle mon json
 retroPlay.controller('consolesCtrl', function($scope , $http, $rootScope){
 
   $http.get('products.json').then(function(response){
